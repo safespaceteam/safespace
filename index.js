ebac97a5ce458e696303c703423e133a86bf216b
ntsum <natsukajiwaram@gmail.com>
2019-03-11 14:23:04 -0400

    created a js file for all site functionality. dropbox for mobile is functioning, just need to mkae it look less ugly

---
+++ index.js
@@ -0,0 +1,15 @@
+class Dropdown {
+    constructor(element) {
+      this.element = element;
+      this.button = this.element.querySelector('.dropdown-button');
+      this.content = this.element.querySelector('.dropdown-content');
+      this.button.addEventListener('click', () => 
+          this.toggleContent())
+    }
+    toggleContent() {
+      this.content.classList.toggle('dropdown-hidden')
+    }
+  }
+  let dropdowns = document.querySelectorAll('.dropdown').forEach( dropdown => new Dropdown(dropdown));
+  
+  
